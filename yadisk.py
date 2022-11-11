c738f61fb216f16c54f1398f8ab832bf232be403
Stepan Rassolov <stepa.rassolov@mail.ru>
2022-11-11 23:34:36 +0500

    second commit

--- yadisk.py
+++ yadisk.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
@@ -10,17 +11,24 @@ class YaUploader:
           'Autorization': f'OAuth {self.token}'       
       }
 
-def upload(self, file_path: str):
+def upload(self, disk_file_path: str):
     files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
     headers = self.get_headers()
-    params = {'path': file_path, 'overwrite': 'true'}
+    params = {'path': disk_file_path, 'overwrite': 'true'}
     response = requests.get(files_url, headers=headers, params=params)
     print(response.json())
     return response.json()
+
+def upload_file_to_disk(self, disk_file_path, filename):
+    href = self.upload(disk_file_path=disk_file_path).get('href', '')
+    response = requests.put(href, data=open(filename, 'rb'))
+    response.raise_for_status()
+    if response.status_code == 201:
+        print('успешно')
       
  
 if __name__ == '__main__':
-    path_to_file = ('C:\Users\minot\Desktop\HTTP\test.txt')
+    path_to_file = ('netology/test.txt', 'test.txt')
     token = 'y0_AgAAAABadk2tAADLWwAAAADTgxJMxtuvJnygS-iv9TrBtvZEAZxVEzw'
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
